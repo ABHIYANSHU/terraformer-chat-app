301cebd48d6355a3d12794578c50f0a289f539e9
abhsriva8 <abhinav.srivastava5@epsilon.com>
2025-05-14 12:14:59 +0530

    push env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api/message',
+  keysApiUrl: 'http://localhost:3000/api/keys',
+  terraformApplyUrl: 'http://localhost:3000/api/terraform/apply'
+};
